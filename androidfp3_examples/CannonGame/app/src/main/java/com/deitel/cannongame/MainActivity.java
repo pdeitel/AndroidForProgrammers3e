e62c79b94609841266b333bc076e3d00b17cd009
LeoAndo <leo.777.info@gmail.com>
2022-03-22 17:40:46 +0900

    Recreate CannonGame App based on the template project of Android Studio Bumblebee

--- androidfp3_examples/CannonGame/app/src/main/java/com/deitel/cannongame/MainActivity.java
+++ androidfp3_examples/CannonGame/app/src/main/java/com/deitel/cannongame/MainActivity.java
@@ -2,8 +2,9 @@
 // MainActivity displays the MainActivityFragment
 package com.deitel.cannongame;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
    // called when the app first launches
--- androidfp3_examples/CannonGame/app/src/main/java/com/deitel/cannongame/MainActivityFragment.java
+++ androidfp3_examples/CannonGame/app/src/main/java/com/deitel/cannongame/MainActivityFragment.java
@@ -4,10 +4,11 @@ package com.deitel.cannongame;
 
 import android.media.AudioManager;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 public class MainActivityFragment extends Fragment {
    private CannonView cannonView; // custom view to display the game
